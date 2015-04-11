3f8390351910778b23f6edc847570951cc9a7981
MagicStick <MagicStick@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-11 18:10:08 +0000

    Initial Commit (2nd Try)
    git-svn-id: https://plugins.svn.wordpress.org/multisite-post-duplicator/trunk@1132837 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mpd.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Plugin Name: 	Multisite Post Duplicator
+Plugin URI: 	http://www.wpmaz.uk
+Description:  	Duplicate/Copy and page/post from one site on a multisite nextwork to another
+Version: 		1.0
+Author: 		Mario Jaconelli
+Author URI:  	http://www.wpmaz.uk
+*/
+
+
+include('inc/load-scripts.php');
+include('inc/admin-ui.php');
+include('inc/core.php');
+
+
+
+
